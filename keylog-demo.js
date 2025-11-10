b9f8c6b04130b6a79d3ff82156001dee34b8a4f1
GhoulSalesman <olof@zerocopter.com>
2025-11-10 10:42:04 +0100

    Add files via upload

--- keylog-demo.js
+++ keylog-demo.js
@@ -1,3 +1,13 @@
-$(document).on("keypress", function(e) {
-        console.log(encodeURIComponent(e.key));
-});
+function handleKey(e) {
+  const key = (typeof e.key === 'string' && e.key.length > 0)
+    ? e.key
+    : String.fromCharCode(e.which || e.keyCode || 0);
+
+  console.log(encodeURIComponent(key));
+}
+
+if (document.addEventListener) {
+  document.addEventListener('keypress', handleKey, false);
+} else if (document.attachEvent) { 
+  document.attachEvent('onkeypress', handleKey);
+}
